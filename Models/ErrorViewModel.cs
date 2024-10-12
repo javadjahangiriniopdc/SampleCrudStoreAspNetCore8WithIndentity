13598a7b8c2bbe76506cd4dbd268342fc28bce19
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-10-12 18:05:22 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleCrudStoreAspNetCore8WithIndentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
